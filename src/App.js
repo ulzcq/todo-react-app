c2c3be1c061caeb712da9697cc1cfb6a1313e912
ulzcq <syj9913@ynu.ac.kr>
2022-10-24 20:29:02 +0900

    Fix: UI 글리치 해결

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, {useEffect, useState} from "react";
 import Todo from './Todo';
 import AddTodo from './AddTodo';
-import { Container, List, Paper } from '@mui/material';
+import { 
+  Container, 
+  List, 
+  Paper,
+  Grid,
+  Button,
+  AppBar,
+  Toolbar,
+  Typography,
+ } from "@mui/material";
 import './App.css';
-import { call } from "./service/ApiService";
+import { call , signout} from "./service/ApiService";
 
 function App() {
   const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    call("/todo", "GET", null)
-    .then((response) => setItems(response.data));
+    call("/todo", "GET", null).then((response) => {
+      setItems(response.data)
+      setLoading(true);
+    });
   },[]);
 
   const editItem = (item) => { //item 매개변수 추가됨
@@ -49,14 +61,46 @@ function App() {
     </Paper>
   );
 
-  return(
-    <div className="App">
-    <Container maxWidth="md">
-      <AddTodo addItem={addItem} />
-      <div className="TodoList">{todoItems}</div>
-    </Container>
-  </div>
+  // navigationBar 추가
+  let navigationBar = (
+    <AppBar position="static">
+      <Toolbar>
+        <Grid justifyContent="space-between" container>
+          <Grid item>
+            <Typography variant="h6">오늘의 할일</Typography>
+          </Grid>
+          <Grid item>
+            <Button color="inherit" raised onClick={signout}>
+              로그아웃
+            </Button>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
   );
+
+   /* 로딩중이 아닐 때 렌더링 할 부분 */
+   let todoListPage = (
+    <div>
+      {navigationBar} {/* 네비게이션 바 렌더링 */}
+      <Container maxWidth="md">
+        <AddTodo addItem={addItem} />
+        <div className="TodoList">{todoItems}</div>
+      </Container>
+    </div>
+  );
+
+  /* 로딩중일 때 렌더링 할 부분 */
+  let loadingPage = <h1> 로딩중.. </h1>;
+  let content = loadingPage;
+
+  if (!loading) {
+    /* 로딩중이 아니면 todoListPage를 선택*/
+    content = todoListPage;
+  }
+
+  /* 선택한 content 렌더링 */
+  return <div className="App">{content}</div>;
 }
 
 export default App;
