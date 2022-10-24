a94a7367c42b19c8e0624e233da72f843d20d197
ulzcq <syj9913@ynu.ac.kr>
2022-10-24 20:31:15 +0900

    Feat: 계정 생성 페이지 구현, 누락된 로그아웃 메서드 추가

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -48,3 +48,12 @@ export function signin(userDTO){
         }
     });
 }
+
+export function signout(){
+    localStorage.setItem("ACCESS_TOKEN", null);
+    window.location.href = "/login";
+}
+
+export function signup(userDTO) {
+    return call("/auth/signup", "POST", userDTO);
+}
